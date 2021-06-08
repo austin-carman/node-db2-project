fbe886ac7a6ab806c2d55543ffc36705000c5cac
Austin Carman <abcarman12@gmail.com>
2021-06-08 15:51:18 -0600

    connected server, model, router and db

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,20 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const { 
+    checkCarId, 
+    checkCarPayload, 
+    checkVinNumberUnique, 
+    checkVinNumberValid 
+} = require('./cars-middleware');
+
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(next)
+})
+
+
+module.exports = router;
